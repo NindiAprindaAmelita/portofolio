d717bd306ee181fdebe8e8813dbfca1e3218135d
Nindi Aprinda Amelita <nindinindi565@gmail.com>
2022-04-24 22:24:50 +0700

    first commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <div className="Footer">
+        <br></br>
+<hr></hr>
+
+<p>Copyright &copy; 2022 Nindi Aprinda A.</p>
+    
+ 
+
+    </div>
+
+  );
+};
+
+export default Footer;
